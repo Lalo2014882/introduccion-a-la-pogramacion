f3479ceb6c50eb11ff579401f658d074e5bfa438
Lalo2014882 <111621728+Lalo2014882@users.noreply.github.com>
2022-08-20 21:17:02 -0500

    Add files via upload

---
+++ programacion/operacionesbasicas.py
@@ -0,0 +1,16 @@
+# -*- coding: iso-8859-15 -*-
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	print "ingresa dos cantidades a operar"
+	n1 = float(raw_input())
+	n2 = float(raw_input())
+	suma = n1+n2
+	resta = n1-n2
+	multi = n1*n2
+	div = n1/n2
+	print "los totales de las operaciones son: ,Suma: ",suma,", Resta:",resta," Multiplcacion: ",multi," Division: ",div
+
